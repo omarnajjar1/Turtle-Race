5980e80c0b9430347ce97185b0f35cbfaac6943f
omarnajjar1 <omarnajjar15.on@gmail.com>
2024-10-29 14:10:18 +0200

    Update Turtle_Race.py

--- Turtle_Race.py
+++ Turtle_Race.py
@@ -1,24 +1,47 @@
 from turtle import Turtle, Screen
 import random
 
+# Display user's screen
 window = Screen()
-window.setup(800, 800)
+window.setup(1100, 800)
+window.bgcolor("black")
 window.title("Turtle Race")
 
+# Make the final white line
+line = Turtle()
+line.hideturtle()
+line.color("white")
+line.penup()
+line.goto(380,-400)
+line.pendown()
+line.left(90)
+line.pensize(20)
+line.forward(800)
+line.speed("fastest")
+
+# First turlte
 sam = Turtle()
 sam.shape('turtle')
+sam.shapesize(2)
 sam.color('red')
 sam.penup()
+sam.goto(-500,-250)
 
+# Second turtle
 tom = Turtle()
 tom.shape('turtle')
 tom.color("blue")
+tom.shapesize(2)
 tom.penup()
+tom.goto(-500,0)
 
+# Third turtle
 jan = Turtle()
 jan.shape('turtle')
 jan.color('green')
+jan.shapesize(2)
 jan.penup()
+jan.goto(-500, 250)
 
 def random_forward():
     return random.choice([2,3,4,5,6,7,8,9,10])
@@ -43,9 +66,6 @@ def display_result(guessing, winner):
     else:
         sam.write("You Lose!", align= "center", font= ("Arial", 30, "bold"))
 
-sam.goto(-300,-250)
-tom.goto(-300,0)
-jan.goto(-300, 250)
 
 while True:
     guessing = window.textinput("Make your bet", "Guess the winner: \nType a color: Red, Blue or Green ?")
