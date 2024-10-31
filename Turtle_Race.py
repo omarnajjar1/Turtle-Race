2a54fa687b7d831f5e6c7ac3bc9d3ea52489659d
omarnajjar1 <omarnajjar15.on@gmail.com>
2024-10-31 09:35:17 +0200

    Update Turtle_Race.py

--- Turtle_Race.py
+++ Turtle_Race.py
@@ -1,82 +1,113 @@
 from turtle import Turtle, Screen
+from pygame import mixer
 import random
+import time
 
-# Display user's screen
-window = Screen()
-window.setup(1100, 800)
-window.bgcolor("black")
-window.title("Turtle Race")
+mixer.init()
 
-# Make the final white line
-line = Turtle()
-line.hideturtle()
-line.color("white")
-line.penup()
-line.goto(380,-400)
-line.pendown()
-line.left(90)
-line.pensize(20)
-line.forward(800)
-line.speed("fastest")
+def game():
+    # Display user's screen
+    window = Screen()
+    window.setup(1100, 800)
+    window.bgcolor("black")
+    window.title("Turtle Race")
 
-# First turlte
-sam = Turtle()
-sam.shape('turtle')
-sam.shapesize(2)
-sam.color('red')
-sam.penup()
-sam.goto(-500,-250)
+    # Draw a gray background
+    grawTurtle = Turtle()
+    grawTurtle.hideturtle()
+    grawTurtle.color("gray")
+    grawTurtle.speed("fastest")
+    grawTurtle.penup()
+    grawTurtle.goto(-550, -400)
+    grawTurtle.begin_fill()
+    grawTurtle.forward(930)
+    grawTurtle.left(90)
+    grawTurtle.forward(800)
+    grawTurtle.left(90)
+    grawTurtle.forward(930)
+    grawTurtle.left(90)
+    grawTurtle.forward(800)
+    grawTurtle.end_fill()
 
-# Second turtle
-tom = Turtle()
-tom.shape('turtle')
-tom.color("blue")
-tom.shapesize(2)
-tom.penup()
-tom.goto(-500,0)
+    # Draw the white finish line
+    whiteTurtle = Turtle()
+    whiteTurtle.hideturtle()
+    whiteTurtle.pencolor('white')
+    whiteTurtle.penup()
+    whiteTurtle.goto(380,-400)
+    whiteTurtle.pendown()
+    whiteTurtle.left(90)
+    whiteTurtle.pensize(20)
+    whiteTurtle.forward(800)
+    whiteTurtle.penup()
+    whiteTurtle.home()
+    whiteTurtle.pendown()
 
-# Third turtle
-jan = Turtle()
-jan.shape('turtle')
-jan.color('green')
-jan.shapesize(2)
-jan.penup()
-jan.goto(-500, 250)
+    # For writing text
+    wrinting_turtle = Turtle()
+    wrinting_turtle.hideturtle()
 
-def random_forward():
-    return random.choice([2,3,4,5,6,7,8,9,10])
+    colors = ('red','blue','green')
+    y_positions = (-250,0,250)
+    turtles = []
 
-def check_result():
-    if sam.xcor() >= 380:
-        return "red"
-    elif tom.xcor() >= 380:
-        return "blue"
-    elif jan.xcor() >= 380:
-        return "green"
+    for i in range(3):
+        new_turtle = Turtle('turtle') 
+        new_turtle.shapesize(2)
+        new_turtle.color(colors[i])
+        new_turtle.penup()
+        new_turtle.goto(-500, y_positions[i])
+        turtles.append(new_turtle)
 
-def display_result(guessing, winner):
-    sam.pendown()  
-    sam.hideturtle()
-    sam.pencolor('white')
-    window.clear()
-    window.bgcolor('DeepPink')
-    sam.home()
-    if guessing.lower() == winner:
-        sam.write("You win!", align= "center", font= ("Arial", 30, "bold"))
-    else:
-        sam.write("You Lose!", align= "center", font= ("Arial", 30, "bold"))
+    def check_result():
+        "Check if one of the three turtles is crossing the white finish line"
+        for turtle in turtles:
+            if turtle.xcor() > 380:
+                return turtle.pencolor()
 
+    def display_result(guessing, winner):
+        "Display the final results"
+        window.clear()
+        window.bgcolor('DeepPink')
+        if guessing == winner:
+            whiteTurtle.write("You win!", align= "center", font= ("Arial", 40, "bold"))
+            mixer.music.load(r"win_sound.mp3")
+        else:
+            whiteTurtle.write("You Lose!", align= "center", font= ("Arial", 40, "bold"))
+            mixer.music.load(r"lose_sound3.mp3")
+        mixer.music.play()
+        time.sleep(2)
+
+
+    is_race_on = True
+    while is_race_on:
+        guessing = window.textinput("Make your bet", "Guess the winner: \nType a color: Red, Blue or Green ?").lower()
+        if guessing in ["blue", "red", "green"]:
+            mixer.music.load(r'starting_game_sound.mp3')  
+            mixer.music.play()
+            time.sleep(3)
+            mixer.music.load(r'exiting_match_sound.mp3')
+            mixer.music.play()
+        elif guessing == 'exit':
+            quit()
+        else:
+            wrinting_turtle.color("orange")
+            wrinting_turtle.write("Invalid Choice", align= "center", font= ("Arial", 30, "bold"))
+            time.sleep(2)
+            wrinting_turtle.clear()
+            continue
+
+        while True:
+            for turtle in turtles:
+                turtle.forward(random.randint(1,8))
+                winner = check_result()
+            if winner:
+                wrinting_turtle.color("green")
+                display_result(guessing, winner)
+                is_race_on = False
+                break
+                
 
 while True:
-    guessing = window.textinput("Make your bet", "Guess the winner: \nType a color: Red, Blue or Green ?")
-    while guessing in ["blue", "red", "green"]:
-        sam.forward(random_forward())
-        tom.forward(random_forward())
-        jan.forward(random_forward())
-        winner = check_result()
-        if winner:
-            display_result(guessing, winner)
-            is_on_race = False
-            break
-
-    window.exitonclick()
+    game()
+    time.sleep(2)
